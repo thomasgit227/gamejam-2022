d6b413b1a15300f98a44d03867d953ad7d6e6dec
Reece Bourgeois <reecebourgeois@gmail.com>
2022-11-05 06:19:59 -0500

    Home page styling started

--- Home.php
+++ Home.php
@@ -1,29 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="./Home.css">
 </head>
+
 <body>
-
-<div class="jumbotron text-center">
-  <h1>Welcome to Souper Chef I</h1>
-  <p>In this instillation of Souper Chef you must help Chef Chad Chaddington balance the flavors of the perfect soup!</p>
-</div>
-
-<div class="col-md-auto text-center">
-    <button id="startGameButton" class="btn btn-primary btn-lg" >
+  <div class="home">
+    <div class="title">
+      Welcome to Souper Chef I
+    </div>
+    <div class="subtitle">
+      In this installation of Souper Chef you must help Chef Chad Chaddington balance the flavors of the perfect soup!
+    </div>
+    <div class="content">
+      <button class="start-button btn btn-primary btn-lg" onclick="test()">
         Start Game!
-    </button>
+      </button>
+    </div>
+  </div>
 
-
-<?php 
-
-?>
-
+  <script>
+    function test() {
+      console.log("Hello World");
+    }
+  </script>
 </body>
+
 </html>
